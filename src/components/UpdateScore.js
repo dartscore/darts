4f1abd1aa6c391c3b38cb20cd394a229abf309ec
matthewbeta <hello@matthew-jackson.com>
2016-08-02 23:07:02 +0100

    Adds functionality to autofocus input on render

--- src/components/UpdateScore.js
+++ src/components/UpdateScore.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+var ReactDOM = require('react-dom');
+
 
 class UpdateScore extends Component {
 
@@ -7,6 +9,10 @@ class UpdateScore extends Component {
     this.state = {
       score: ''
     }
+  }
+
+  componentDidMount = () => {
+    ReactDOM.findDOMNode(this.refs.amountScored).focus(); 
   }
 
   handleSubmit = (event) => {
@@ -21,17 +27,18 @@ class UpdateScore extends Component {
 
   render() {
     return (
-      <form className="pure-form update-score"  onSubmit={this.handleSubmit}>
+      <form className="form update-score"  onSubmit={this.handleSubmit}>
 
-        <label htmlFor="amountScored">You scored</label>
-        <input id="amountScored" ref="amountScored" type="number" placeholder="0"
-          min={this.props.min}
-          max={this.props.max}
-          value={this.state.score}
-          onChange={this.handleChange} />
+        <div className="form__row">
+          <label htmlFor="amountScored" className="form__label">You scored</label>
+          <input id="amountScored" ref="amountScored" type="number" placeholder="0"
+            min={this.props.min}
+            max={this.props.max}
+            value={this.state.score}
+            onChange={this.handleChange} className="form__field" />
+        </div>
 
-
-        <button type="submit" className="pure-button pure-button-primary" >Score</button>
+        <button type="submit" className="btn">Add Score</button>
       </form>
     );
   }
